0bbec92d7803b059cc97aeddfe3d7e1f8cab9048
Menzy <joshuamensah46@gmail.com>
2025-02-15 08:24:52 +0000

    Initial Commit

---
+++ timelapse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  timelapse
+//
+//  Created by Wan Menzy on 2/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ timelapse/timelapseApp.swift
@@ -0,0 +1,17 @@
+//
+//  timelapseApp.swift
+//  timelapse
+//
+//  Created by Wan Menzy on 2/15/25.
+//
+
+import SwiftUI
+
+@main
+struct timelapseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
